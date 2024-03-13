cd47d1014654f18e568377968c301ace5d1e0d88
Obaid <sayed.hariyal@ext.quantiphi.com>
2024-03-13 06:56:11 +0530

    Add a function to print right triangle

--- Patterns.java
+++ Patterns.java
@@ -15,7 +15,21 @@ public class Patterns {
         }
     }
 
-
+    public static void rightTriangle(int n) {
+        /**
+         * This function will print the right angle triangle
+         *  * 
+            * * 
+            * * *
+            * * * *
+         */
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         solidRectangle(5);
     }
