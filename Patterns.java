da41fa6103926833d0d9b2406d55fcc5113f3479
Obaid <sayed.hariyal@ext.quantiphi.com>
2024-03-13 07:00:43 +0530

    Add a function to print right angle triangle with numbers

--- Patterns.java
+++ Patterns.java
@@ -30,6 +30,22 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void numberTriangle(int n){
+        /**
+         * This function will print the right angle triangle with numbers
+         *  1 
+            1 2 
+            1 2 3
+            1 2 3 4
+         */
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         solidRectangle(5);
     }
