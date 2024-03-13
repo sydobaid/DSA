8322094039a4aa054513b1d88b3bac76e26577dc
Obaid <sayed.hariyal@ext.quantiphi.com>
2024-03-13 06:46:45 +0530

    Add a function to print solid rectangle

--- Patterns.java
+++ Patterns.java
@@ -1,4 +1,22 @@
 public class Patterns {
+
+    public static void solidRectangle(int n) {
+        /**
+         * This function will print the solid rectangle
+         *  * * *
+            * * *
+            * * *
+         */
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+
     public static void main(String[] args) {
+        solidRectangle(5);
     }
 }
